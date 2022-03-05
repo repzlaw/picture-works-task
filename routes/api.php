973a178662f78c781578e3bfd3b840063e54133f
repzlaw <ibitoyedavid@gmail.com>
2022-03-06 00:04:01 +0100

    added routes for toggle status routes

--- routes/api.php
+++ routes/api.php
@@ -14,14 +14,13 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::apiResources([
     '/tasks'=> 'TaskController',
-
 ]);
+
+Route::put('/tasks/{task}/toggle-status',[App\Http\Controllers\TaskController::class,'toggleTaskCompletedStatus']);
+
+
 
 
 Route::fallback(function(){
