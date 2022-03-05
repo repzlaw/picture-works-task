2065abc81d55f4a3044852929a71a45619639cf4
repzlaw <ibitoyedavid@gmail.com>
2022-03-06 00:00:42 +0100

    created accessors and boot method for sort order

--- app/Task.php
+++ app/Task.php
@@ -7,4 +7,18 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     protected $fillable = ['label', 'sort_order', 'completed_at'];
+
+    public static function boot() {
+	    parent::boot();
+	    static::created(function($task) {
+	        $task->update(['sort_order'=>$task->id]);
+	    });
+    }
+
+    public function getCreatedAtAttribute($value){
+        return date('jS, F Y.',strtotime($value));
+    }
+    public function getCompletedAtAttribute($value){
+        return  $value ? date('jS, F Y H:i A.',strtotime($value)) : null;
+    }
 }
